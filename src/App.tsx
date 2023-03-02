38f6d612035ff4c6405948e0ba9e0cf182928583
NghiepRadeon <nghiepradeon@gmail.com>
2023-03-02 23:15:59 +0700

    add socket.io-client and test

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import io from "socket.io-client";
+import "./App.css";
 
+const SOCKET_URL = "http://localhost:5000";
+const socket = io(SOCKET_URL);
 function App() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    socket.on("connect", () => {
+      console.log("Connected to server");
+    });
+
+    socket.on("newConnection", (data) => {
+      console.log(data);
+    });
+
+    socket.on("disconnect", () => {
+      console.log("Disconnected from server");
+    });
+
+    socket.on("message", (data) => {
+      setMessage(data);
+    });
+
+    socket.on("reply", (message) => {
+      console.log(message);
+    });
+  }, []);
+
+  const handleSend = () => {
+    socket.emit("message", "Hello from client!");
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Message received: {message}</p>
+        <button onClick={handleSend}>Send message</button>
       </header>
     </div>
   );
